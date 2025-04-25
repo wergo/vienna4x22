0cd8b86dac41fd96444aa9b8a12105cc39c3ff45
Werner Goebl <goebl@mdw.ac.at>
2025-04-25 15:23:17 +0200

    Add expanded Mozart

---
+++ mei/expand.sh
@@ -0,0 +1,2 @@
+verovio --expand 'expansion-default' -a -t mei -o Mozart-KV331_1-expanded.mei Mozart-KV331_1.mei
+verovio --expand 'expansion-default' -a -t timemap -o Mozart-KV331_1-expanded.json Mozart-KV331_1.mei
