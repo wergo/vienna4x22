363cc591f4cfd924b7c32638082a3f545356c027
Werner Goebl <goebl@mdw.ac.at>
2025-04-25 15:29:28 +0200

    Add expanded Schubert

--- mei/expand.sh
+++ mei/expand.sh
@@ -1,2 +1,4 @@
 verovio --expand 'expansion-default' -a -t mei -o Mozart-KV331_1-expanded.mei Mozart-KV331_1.mei
 verovio --expand 'expansion-default' -a -t timemap -o Mozart-KV331_1-expanded.json Mozart-KV331_1.mei
+verovio --expand 'expansion-default' -a -t mei -o Schubert_783_15-expanded.mei Schubert_783_15.mei
+verovio --expand 'expansion-default' -a -t timemap -o Schubert_783_15-expanded.json Schubert_783_15.mei
